c2952be7ba7d0903de98e84622acb466436c2105
sanjaybhati87 <sanjaythakur287@gmail.com>
2016-03-18 10:41:47 +0530

    New code added to JDBC
    New code added to JDBC

--- src/JDBC.java
+++ src/JDBC.java
@@ -7,6 +7,9 @@ public class JDBC {
 		private static Connection conn=null;
 		private static final String Driver = "oracle.jdbc.driver.OracleDriver";
 		private static final String ConnectionString= "jdbc:oracle:thin:QA_Automation_Mobile/QA_Automation_Mobile145@10.105.36.139:1525:SEPQA01";
+		private static final String user="user";
+		private static final String pwd = "password";
+		
 		
 		
 		
