f6d16a5bd2a02d686c9d1b19b43a11c9d744ec41
Sanjay Singh 8 <sanjaythakur287@gmail.com>
2016-03-18 09:55:11 +0530

    Change in JDBC code

--- src/JDBC.java
+++ src/JDBC.java
@@ -1,9 +1,17 @@
+import java.sql.Connection;
 
 public class JDBC {
 
-	public static void main(String[] args) {
+	
 		// TODO Auto-generated method stub
+		private static Connection conn=null;
+		private static final String Driver = "oracle.jdbc.driver.OracleDriver";
+		private static final String ConnectionString= "jdbc:oracle:thin:QA_Automation_Mobile/QA_Automation_Mobile145@10.105.36.139:1525:SEPQA01";
+		
+		
+		
+		
 
-	}
+	
 
 }
